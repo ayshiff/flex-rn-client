7fbb49b01a84994ea592e325d64213a29c1a7ed9
ayshiff <remi.d45@gmail.com>
2018-12-21 15:15:07 +0100

    improve: update tests

--- __tests__/Leave/Leave_test.js
+++ __tests__/Leave/Leave_test.js
@@ -1,13 +1,13 @@
 import "react-native";
 import React from "react";
-import renderer from "react-test-renderer";
+import * as enzyme from "enzyme";
+import ReactSixteenAdapter from "enzyme-adapter-react-16";
 import LeaveScreen from "../../Components/Leave/LeaveScreen";
+
+enzyme.configure({ adapter: new ReactSixteenAdapter() });
 
 const navigation = { navigate: jest.fn(), popToTop: jest.fn() };
 
 it("renders correctly", () => {
-  const tree = renderer
-    .create(<LeaveScreen navigation={navigation} />)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+  enzyme.shallow(<LeaveScreen navigation={navigation} />);
 });
