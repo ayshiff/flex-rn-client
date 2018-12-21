6cbdeed06d2d161fbb5032dc74feb17967549e7c
ayshiff <remi.d45@gmail.com>
2018-12-21 15:15:07 +0100

    improve: update tests

--- __tests__/Login/Login_test.js
+++ __tests__/Login/Login_test.js
@@ -1,14 +1,14 @@
 import "react-native";
 import React from "react";
-import renderer from "react-test-renderer";
+import * as enzyme from "enzyme";
+import ReactSixteenAdapter from "enzyme-adapter-react-16";
 import LoginScreen from "../../Components/Login/LoginScreen";
 import "isomorphic-fetch";
+
+enzyme.configure({ adapter: new ReactSixteenAdapter() });
 
 const navigation = { navigate: jest.fn(), popToTop: jest.fn() };
 
 it("renders correctly", () => {
-  const tree = renderer
-    .create(<LoginScreen navigation={navigation} />)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+  enzyme.shallow(<LoginScreen navigation={navigation} />);
 });
