0a5c15898ef36653f020811ef630737392f3925c
ayshiff <remi.d45@gmail.com>
2018-12-26 17:34:04 +0100

    fix: lint error

--- Components/Profile/Users/UsersScreen.js
+++ Components/Profile/Users/UsersScreen.js
@@ -1,5 +1,5 @@
 // @flow
-// /* eslint-disable */
+/* eslint-disable */
 import React from "react";
 import { FormInput, ListItem } from "react-native-elements";
 import Icon from "react-native-vector-icons/FontAwesome";
