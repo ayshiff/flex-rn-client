63175dea553e7921d5ff08f030da4f52aaca8c40
ayshiff <remi.d45@gmail.com>
2019-01-04 11:55:06 +0100

    improve: update Places Screen Design

--- Components/Profile/Places/components/FetchPlacesButton.js
+++ Components/Profile/Places/components/FetchPlacesButton.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, TouchableOpacity } from "react-native";
+import { View, TouchableOpacity, Text } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 import styles from "./styles/FetchPlacesButtonStyle";
 
@@ -8,6 +8,15 @@ const FetchPlacesButton = (props: { onPress: () => void }) => {
   return (
     <View style={styles.view}>
       <TouchableOpacity onPress={onPress} style={styles.container}>
+        <Text
+          style={{
+            color: "#2E89AD",
+            fontFamily: "Raleway",
+            fontWeight: "bold"
+          }}
+        >
+          Je recherche une place
+        </Text>
         <Icon name="arrow-right" size={15} color="#2E89AD" />
       </TouchableOpacity>
     </View>
